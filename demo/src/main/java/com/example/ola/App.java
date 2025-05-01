b6b83ea1416caf66ad717cf8d4e08188033a999a
Douglas-Pio <douglaspiofernandes@hotmail.com>
2025-04-30 23:50:23 -0300

    Commit Hello world com Junit

---
+++ demo/src/main/java/com/example/ola/App.java
@@ -0,0 +1,9 @@
+package com.example.ola;
+
+
+
+public class OlaMundo {
+	public String olaMundo() {
+		return "Hello, World!";
+	}
+}
---
+++ demo/src/test/java/com/example/ola/AppTest.java
@@ -0,0 +1,16 @@
+package com.example.ola;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OlaMundoTeste {
+
+	@Test
+	void testOlaMundo() {
+		OlaMundo olaMundo = new OlaMundo();
+		assertEquals("Hello, World!", olaMundo.olaMundo());
+	}
+
+}
